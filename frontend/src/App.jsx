1f94980ad1f45db0b9cafc4bb909bfb22662deb3
pushkarj7 <pushkarj3110@gmail.com>
2026-09-21 22:07:39 +0530

    Adding file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,17 @@
- import { useState } from 'react'
+// import { useState } from 'react'
 // import heroImg from './assets/hero.png'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 import './App.css'
+import Home from './pages/home'
 
 function App() {
-let [counter,setCounter]=useState(0);
-  
-if(counter=>20){
-  let addValue =() =>{
-setCounter(counter+1)
-}
-}
-if(counter<=0){
-let removeValue =() =>{
-setCounter(counter-1);
-}
+return(
+  <Home />
+);
 
 }
-  return (
-    <>
-   <h2 className="text-4xl font-bold text-blue-600">
-    Event Hive
-   </h2>
-   <h3>counter value:{counter}</h3>
-   <button onClick={addValue}>Increment</button>
-   <br/>
-   <button onClick={removeValue}>Decrement</button>
-   </>
-  )
-}
+ 
+
 
 export default App
